a7cf30c00ef0d86edd474a05fe2ab73c1664e2e1
maitungmn <goingdownfr@gmail.com>
2019-07-03 19:28:30 +0700

    add axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import './plugins/vuetify'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
